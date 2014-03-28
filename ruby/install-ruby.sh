118132b994e35e02bf1a37043c7d810a1b888a84
Glauco Vinicius <gl4uc0@gmail.com>
2014-03-28 13:52:40 -0300

    Several changes

--- ruby/install-ruby.sh
+++ ruby/install-ruby.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-rvm install 2.0
-rvm use --default 2.0
+grep -c 2.0 <(rvm list)
+
+if ! $?
+then
+  echo '[ Installing Ruby 2.0 using RVM ]'
+  rvm install 2.0
+  rvm use --default 2.0
+else
+  echo '[ Ruby 2.0 is already installed ]'
+fi
--- ruby/install-rvm.sh
+++ ruby/install-rvm.sh
@@ -1,6 +1,9 @@
-# Installs RVM, if necessary
-if hash rvm 2> /dev/null; then
-    echo >&2 "RVM is already installed";
+#!/bin/sh
+
+if test ! $(which rvm)
+then
+  echo '[ Installing RVM ]'
+  curl -sSL https://get.rvm.io | bash -s stable
 else
-    curl -sSL https://get.rvm.io | bash -s stable
+  echo '[ RVM is already installed ]'
 fi
